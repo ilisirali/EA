a5be7076cfd4711459f910874b58ae001e7e5bb8
Test User <test@example.com>
2026-03-02 23:06:57 +0100

    Optimize iOS webview native shell settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,6 +6,11 @@ const config: CapacitorConfig = {
   webDir: 'dist',
   server: {
     cleartext: true
+  },
+  ios: {
+    contentInset: 'always',
+    allowsLinkPreview: false,
+    scrollEnabled: false // Bu ayar iOS'da ekranın bir web sitesi gibi yukarı aşağı sekmesini engelleyip Native uygulama hissi verir.
   }
 };
 
